e5e84bdb2d28d8f53796f69860792df55c8bf8d3
Fernando Pinheiro <maxotus@gmail.com>
2022-03-14 23:09:16 -0300

    mudança
    mudança

--- imc.py
+++ imc.py
@@ -1,24 +1,33 @@
+'''Calcule seu IMC
+paramentros:
+- 18.5: abaixo do peso
+- 18,5 e 25: peso ideal
+- 25 até 30: sobrepeso
+- 30 até 40: obesidade
+- acima de 40: obesidade mórbida'''
 
-#Calculando o IMC :
-# abaixo de 18.5: Abaixo do peso
-# entre 18,5 e 25: Peso ideal
-# 25 até 30: Sobrepeso
-# 30 até 40: Obesidade
-# acima de 40: Obesidade mórbida
 
+from time import sleep
 
-m  =  float ( input ( "Digite seu peso (em kg): \n " ))
-h  =  float ( input ( "Digite sua altura (em m): \n " ))
+massa  =  float ( input ( "Digite seu peso (em kg): " ))
+altura  =  float ( input ( "Digite sua altura (em m):  " ))
 
-imc  = ( m  / ( h ** 2 ))
+imc  = ( massa  / ( altura ** 2 ))
+
+print('Calculando')
+sleep(0.50)
+print('Seu')
+sleep(0.50)
+print('Imc')
+sleep(0.50)
 
 if  imc  <  18.5:
-    print ( "Seu IMC é de: {:.1f}. Você está abaixo do peso. " . format ( imc ))
+    print ( "Seu IMC é de: {:.1f}. \33[1;33m Atenção abaixo do peso. " . format ( imc ))
 elif  imc < 25:
-    print ( "Seu IMC é de: {:.1f}. Você está no peso ideal." . format ( imc ))
+    print ( "Seu IMC é de: {:.1f}. \33[1;32m Você está no peso ideal." . format ( imc ))
 elif  imc < 30:
-    print ( "Seu IMC é de: {:.1f}. Você está com sobrepeso." . format ( imc ))
+    print ( "Seu IMC é de: {:.1f}. \33[1;35m Você está com sobrepeso." . format ( imc ))
 elif  imc  < 40:
-    print ( "Seu IMC é de : {:.1f}. Você está com obesidade." . format ( imc ))
+    print ( "Seu IMC é de : {:.1f}. \33[1;31m Você está com obesidade." . format ( imc ))
 else:
-    print ( "Seu IMC é de {:.1f}. Você está com obesidade mórbida." . format ( imc ))
+    print ( "Seu IMC é de {:.1f}. \33[4;31m Você está com obesidade mórbida." . format ( imc ))
